1ce169d922ff6e8ec2ddd96990ea6d6b7d6dca70
Miles Morris <milesmorris123@gmail.com>
2023-10-19 18:44:55 +0100

    have got api response to show on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [catFactArray, setCatFactArray] = useState<string[]>([]);
+
+  function getCatFacts() {
+    fetch("https://meowfacts.herokuapp.com/?count=10")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCatFactArray(data.data);
+        // const factsArray = Object.values(data.data) as string[];
+        // setCatFactArray(factsArray);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getCatFacts}>Get Facts</button>
+      {catFactArray.map((fact, index) => (
+        <p key={index}>{fact}</p>
+      ))}
     </div>
   );
 }
