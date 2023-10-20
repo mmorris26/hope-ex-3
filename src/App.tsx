2262a22fb433375d9b4bd440c214f96b0933ac29
Miles Morris <milesmorris123@gmail.com>
2023-10-20 10:49:05 +0100

    got the api to show the images and text on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,46 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [catFactArray, setCatFactArray] = useState<string[]>([]);
+  type Recipe = {
+    name: string;
+    image: string;
+    cuisine: string;
+  };
 
-  function getCatFacts() {
+  type RecipeHit = {
+    recipe: {
+      label: string;
+      image: string;
+      cuisineType?: string;
+    };
+  };
+
+  const [recipeArray, setRecipeArray] = useState<Recipe[]>([]);
+
+  function getRecipes() {
     fetch(
       "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=1d11e1e9&app_key=a0a011287d9011f1443c97fecb290044"
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.hits);
-        // setCatFactArray(data.hits);
+        const newRecipes = data.hits.map((hit: RecipeHit) => ({
+          name: hit.recipe.label,
+          image: hit.recipe.image,
+          cuisine: hit.recipe.cuisineType || "Unknown",
+        }));
+        setRecipeArray(newRecipes);
       });
   }
 
   return (
     <div className="App">
-      <button onClick={getCatFacts}>Get Facts</button>
-      {catFactArray.map((fact, index) => (
-        <p key={index}>{fact}</p>
+      <button onClick={getRecipes}>Get Recipes</button>
+      {recipeArray.map((recipe, index) => (
+        <div key={index} className="recipe">
+          <h2>{recipe.name}</h2>
+          <img src={recipe.image} alt={recipe.name} />
+          <p>Cuisine: {recipe.cuisine}</p>
+        </div>
       ))}
     </div>
   );
